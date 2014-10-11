7edd7c061ad18859abd2618788f759e7b3f21a24
Ranjay Salmon <ranjy23@yahoo.com>
2014-10-11 11:04:07 -0400

    Retrieve friends from parse

--- SharedSpace/EditFriendsTableViewController.m
+++ SharedSpace/EditFriendsTableViewController.m
@@ -16,85 +16,151 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    // declare class variable array in header called *allUsers
     
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
+    // query all users in the parse database
+    PFQuery *query = [PFUser query];
+    [query orderByAscending:@"username"]; // order in alphabetical order by user name
+    // run query
+    [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
+        if (error) {
+            // Error display errory message
+            
+            
+            NSLog(@"Error: %@ %@", error, [error userInfo]);
+            
+        }else{
+            // else success
+            // storing the users returned into the NSArray we declared in our class
+            self.allUsers = objects;
+            
+            
+            // Due to asynchoneous request
+            // let the tableview know that data is ready
+            // by sending the following message
+            [self.tableView reloadData];
+            
+            
+            
+        }
+    }];
     
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    // setting current user
+    self.currentUser = [PFUser currentUser];
+    
+    // setting the friends relation
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
+
 
 #pragma mark - Table view data source
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-#warning Potentially incomplete method implementation.
     // Return the number of sections.
-    return 0;
+    return 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-#warning Incomplete method implementation.
+    
     // Return the number of rows in the section.
-    return 0;
+    return self.allUsers.count;
 }
 
-/*
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    static NSString * cellIdentifier = @"EditFCell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier forIndexPath:indexPath];
     
-    // Configure the cell...
+    // return the array of users into a PFuser object
+    PFUser *user = [self.allUsers objectAtIndex:indexPath.row];
+    
+    cell.textLabel.text = user.username;
+    
+    if ([self isFriend:user]) {
+        cell.accessoryType = UITableViewCellAccessoryCheckmark;
+        
+    }else{
+        
+        cell.accessoryType = UITableViewCellAccessoryNone;
+        
+    }
     
     return cell;
 }
-*/
 
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
+#pragma  mark - Table view Delegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    // allow the selected field to disappear
+    [self.tableView deselectRowAtIndexPath:indexPath animated:NO];
+    
+    
+    // Adding check mark to add users
+    
+    // get reference to the current table view cell
+    UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
+    
+    
+    
+    PFUser *user = [self.allUsers objectAtIndex:indexPath.row];
+    
+    // setting the friends relation object
+    PFRelation *friendsRelation = [self.currentUser relationForKey:@"friendsRelation"];
+    
+    if ([self isFriend:user]) {
+        // remove them
+        // 1. remove checkmark
+        cell.accessoryType = UITableViewCellAccessoryNone;
+        
+         NSLog(@"Not Friend: %@", user.username);
+        
+        // 2. remove from array of friends
+        for (PFUser *friend in self.friends) {
+            if ([friend.objectId isEqualToString:user.objectId]) {
+                [self.friends removeObject:friend];
+                break;
+            }
+        }
+        
+        // 3. remove from the backend
+        [friendsRelation removeObject:user];
+        
+    }else{
+        
+        cell.accessoryType = UITableViewCellAccessoryCheckmark;
+        
+        // adding user back to array
+        [self.friends addObject:user];
+        
+        // adding the user relation
+        [friendsRelation addObject:user];
+        
+        NSLog(@"Friends: %@", self.friends);
+        
+    }
+    // saving the relation to the backend
+    [self.currentUser saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+        if (error) {
+            NSLog(@"Error %@ %@", error, [error userInfo]);
+        }
+    }];
 }
-*/
 
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
+#pragma mark - Helper Methods
+
+- (BOOL) isFriend:(PFUser *)user{
+    
+    // search through the list of friends received
+    // and check if the object id of the friend selected matches
+    // the object id of the friends in the friends list
+    for (PFUser *friend in self.friends) {
+        if ([friend.objectId isEqualToString:user.objectId]) {
+            NSLog(@"Friends are: %@",self.friends);
+            
+            return YES;
+        }
+    }
+    return NO;
 }
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
