f5452438da95ec57a532cde41546a6c50a844ac0
Ranjay Salmon <ranjy23@yahoo.com>
2014-10-11 00:37:28 -0400

    Linked storybord to classes

---
+++ SharedSpace/InboxViewController.h
@@ -0,0 +1,13 @@
+//
+//  InboxViewController.h
+//  SharedSpace
+//
+//  Created by Ranjay Salmon on 10/11/14.
+//  Copyright (c) 2014 Ranjay Salmon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InboxViewController : UIViewController
+
+@end
---
+++ SharedSpace/SignUpViewController.h
@@ -0,0 +1,13 @@
+//
+//  SignUpViewController.h
+//  SharedSpace
+//
+//  Created by Ranjay Salmon on 10/11/14.
+//  Copyright (c) 2014 Ranjay Salmon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SignUpViewController : UIViewController
+
+@end
